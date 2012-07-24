7d82c860334c4e927cc50db962ce397ea64561a7
Cody Horst <chorst22@gmail.com>
2012-07-24 01:11:38 -0700

    Fixed Directory

---
+++ Documents/Eclipse/SBOLConverter/src/org/sbols/converter/rsbpml/Part_List.java
@@ -0,0 +1,34 @@
+/*
+ * Defines leaves and classes within <part_list>
+ */
+
+    package org.sbols.converter.rsbpml;
+	import javax.xml.bind.annotation.XmlElement;
+	import javax.xml.bind.annotation.XmlRootElement;
+	import org.sbolstandard.core.DnaComponent;
+
+	@XmlRootElement(name="part_list")
+	public class Part_List {
+		
+		private Part part;
+		
+	    @XmlElement(name="part")
+	    public Part getPart() {
+	        return part;
+	    }
+
+	    public void setPart(Part part) {
+	        this.part = part;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "part_list [" + (part != null ? part : "") + "]";
+	    }
+	    
+	    public DnaComponent toSbol(DnaComponent biobrick) {
+	    	return part.toSbol(biobrick);
+	    }
+
+	}
+
---
+++ Documents/Eclipse/SBOLConverter/src/org/sbols/converter/rsbpml/Rsbpml.java
@@ -0,0 +1,36 @@
+/*
+ * Defines leaves and classes within <rsbpml>
+ */
+
+package org.sbols.converter.rsbpml;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.sbolstandard.core.DnaComponent;
+import org.sbolstandard.core.SBOLFactory;
+
+@XmlRootElement(name="rsbpml")
+public class Rsbpml {
+	
+	Part_List part_list;
+	
+    @XmlElement(name="part_list")
+    public Part_List getPart_list() {
+        return part_list;
+    }
+
+    public void setPart_list(Part_List part_list) {
+        this.part_list = part_list;
+    }
+
+    @Override
+    public String toString() {
+        return "rsbpml [" + (part_list != null ? part_list : "") + "]";
+    }
+    
+    public DnaComponent toSbol(){
+    	DnaComponent biobrick = SBOLFactory.createDnaComponent();
+    	return part_list.toSbol(biobrick);
+    }
+  
+}
