88888d20f0fdd8e53acbcc663e952dce00d3f438
Cody Horst <chorst22@gmail.com>
2012-08-02 12:32:29 -0700

    Fixed dnaSequenceData Null Exception

--- src/org/sbols/converter/rsbpml/dnaSequenceData.java
+++ src/org/sbols/converter/rsbpml/dnaSequenceData.java
@@ -7,25 +7,31 @@ import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.sbolstandard.core.DnaSequence;
+import org.sbolstandard.core.SBOLFactory;
 
 public class dnaSequenceData {
 	
-	private DnaSequence sequenceObject;
-	private String seq_data;
+	private DnaSequence sequenceObject = SBOLFactory.createDnaSequence();
+	private String nucleotides;
 	
 	@XmlJavaTypeAdapter(CollapsedStringAdapter.class)
     @XmlElement(name="seq_data")
-	public String getNuleotides(){
-		return seq_data;
+	public String getNucleotides(){
+		return nucleotides;
 	}
 	
-	public void setNucleotides(String seq_data){
-		this.seq_data = seq_data;
+	public void setNucleotides(String sequenceData){
+		this.nucleotides = sequenceData;
+	}
+	
+	@Override
+	public String toString(){
+		 return (nucleotides != null ? "Nucleotides: " + nucleotides : "") +  "]";
 	}
 	
 	public DnaSequence toSbol(){
+		sequenceObject.setNucleotides(nucleotides);
 		sequenceObject.setURI(URI.create("http://example.com/MyDnaComponentSequence")); //Need to make dynamic
-		sequenceObject.setNucleotides(seq_data);
 		return sequenceObject;
 	}
 }
