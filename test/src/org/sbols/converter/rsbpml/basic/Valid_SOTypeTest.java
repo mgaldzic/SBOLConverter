21e1af4becc39d1e8d6e477ab0ab60d23dbf9652
Michal Galdzicki <mgaldzic@gmail.com>
2012-12-26 20:01:07 -0800

    binned some tests into Basic package to organize, these tests pass

---
+++ test/src/org/sbols/converter/rsbpml/basic/Valid_SOTypeTest.java
@@ -0,0 +1,32 @@
+package org.sbols.converter.rsbpml.basic;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import org.junit.Test;
+import org.sbols.converter.rsbpml.Rsbpml;
+import org.sbols.converter.rsbpml.Rsbpml;
+import org.sbols.converter.util.ReadXML;
+import org.sbolstandard.core.DnaComponent;
+import org.sbolstandard.core.SBOLDocument;
+import org.sbolstandard.core.SBOLFactory;
+
+public class Valid_SOTypeTest {
+
+    @Test
+    public void Valid_SOTypeTest() throws JAXBException, IOException {
+        System.out.println("Valid_SOTypeTest");
+        Rsbpml rsbpmlData = ReadXML.file("test/data/basic/Valid_SOTypeTest.xml");
+
+        DnaComponent biobrick = SBOLFactory.createDnaComponent();
+        SBOLDocument Doc = SBOLFactory.createDocument();
+        biobrick = rsbpmlData.toSbol();
+        Doc.addContent(biobrick);
+        SBOLFactory.validate(Doc);
+
+        //Remove this later
+        FileOutputStream out = new FileOutputStream("output5.txt");
+        SBOLFactory.write(Doc, out);
+        System.out.println(rsbpmlData);
+    }
+}
