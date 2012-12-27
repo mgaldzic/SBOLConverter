339a94320bad8802380ffc68f899ac3c0ece689d
Michal Galdzicki <mgaldzic@gmail.com>
2012-12-26 23:17:01 -0800

    Valid SpecifiedSubscars Test passes

--- test/src/org/sbols/converter/rsbpml/subparts/Valid_SpecifiedSubcarsTest.java
+++ test/src/org/sbols/converter/rsbpml/subparts/Valid_SpecifiedSubcarsTest.java
@@ -4,13 +4,13 @@
  */
 package org.sbols.converter.rsbpml.subparts;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import org.junit.Ignore;
+import java.io.FileNotFoundException;
+import javax.xml.bind.JAXBException;
 import org.junit.Test;
-import org.sbols.converter.rsbpml.specified_subscars;
-import org.sbols.converter.rsbpml.subpart;
-import org.sbolstandard.core.DnaComponent;
+import org.sbols.converter.rsbpml.Rsbpml;
+import org.sbols.converter.util.ReadXML;
+import org.sbolstandard.core.SBOLDocument;
+import org.sbolstandard.core.SBOLFactory;
 
 /**
  *
@@ -18,45 +18,12 @@ import org.sbolstandard.core.DnaComponent;
  */
 public class Valid_SpecifiedSubcarsTest {
     
-    @Ignore
     @Test
-    public void Valid_SpecifiedSubcarsTest() {
+    public void Valid_SpecifiedSubcarsTest() throws JAXBException, FileNotFoundException {
         System.out.println("Valid_SpecifiedSubcarsTest");
-        specified_subscars instance = new specified_subscars();
-        subpart expResult = null;
-        subpart result = instance.getSubpart();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setSubpart method, of class specified_subscars.
-     */
-    @Ignore
-    @Test
-    public void testSetSubpart() {
-        System.out.println("setSubpart");
-        subpart subpart = null;
-        specified_subscars instance = new specified_subscars();
-        instance.setSubpart(subpart);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of toSbol method, of class specified_subscars.
-     */
-    @Ignore
-    @Test
-    public void testToSbol() {
-        System.out.println("toSbol");
-        DnaComponent biobrick = null;
-        specified_subscars instance = new specified_subscars();
-        DnaComponent expResult = null;
-        DnaComponent result = instance.toSbol(biobrick);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Rsbpml rsbpmlData = ReadXML.file("test/data/subparts/Valid_SpecifiedSubcarsTest.xml");
+        SBOLDocument Doc = SBOLFactory.createDocument();
+        Doc.addContent(rsbpmlData.toSbol());
+        SBOLFactory.validate(Doc);
     }
 }
