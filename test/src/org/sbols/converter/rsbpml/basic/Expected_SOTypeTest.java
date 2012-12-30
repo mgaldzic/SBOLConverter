67af95361f6c30e56d525d477afebe85e7875480
Michal Galdzicki <mgaldzic@gmail.com>
2012-12-30 14:26:29 -0800

    added Expected SOType Test - single SO

---
+++ test/src/org/sbols/converter/rsbpml/basic/Expected_SOTypeTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sbols.converter.rsbpml.basic;
+
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.sbols.converter.SBOLConverter;
+import org.sbols.converter.rsbpml.Rsbpml;
+import org.sbols.converter.util.ReadFile;
+import org.sbols.converter.util.ReadXML;
+import org.sbolstandard.core.SBOLDocument;
+
+/**
+ *
+ * @author mgaldzic
+ */
+public class Expected_SOTypeTest {
+    @Test
+    public void Expected_SOTypeTest() throws JAXBException, IOException {
+        System.out.println("Expected_SOTypeTest");
+        //Get input for test
+        Rsbpml rsbpmlData = ReadXML.file("test/data/basic/Valid_SOTypeTest.xml");
+
+        //Do the test
+        SBOLDocument SbolDoc = SBOLConverter.convert(rsbpmlData);
+        String actual = ReadFile.sbolDocToString(SbolDoc);
+        
+        //Get expected result
+        String expected = ReadFile.fromPath("test/data/basic/Valid_SOTypeTest.sbol.xml");
+
+        //Compare
+        assertTrue("Diffs found: ", ReadFile.compare(expected, actual));
+    }
+    
+}
