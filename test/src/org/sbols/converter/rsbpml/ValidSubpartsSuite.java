1dda5707435231b8eb613622bfd39a2b8a904490
Michal Galdzicki <mgaldzic@gmail.com>
2012-12-31 01:21:45 -0800

    new suite for Valid Subparts tests

---
+++ test/src/org/sbols/converter/rsbpml/ValidSubpartsSuite.java
@@ -0,0 +1,47 @@
+package org.sbols.converter.rsbpml;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.sbols.converter.rsbpml.subparts.*;
+
+/**
+ *
+ * @author mgaldzic
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    Valid_SpecifiedSubpartsTest.class,
+    Valid_SpecifiedSubpartsSubscarsTest.class, 
+    Valid_DeepSubpartTest.class,
+    Valid_MultiDeepSubpartsTest.class
+})
+public class ValidSubpartsSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
