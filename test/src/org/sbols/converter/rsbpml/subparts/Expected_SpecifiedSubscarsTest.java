8ae2cdddeaee5f1d17bf602fbb3dd4626779f169
Michal Galdzicki <mgaldzic@gmail.com>
2012-12-31 02:32:40 -0800

    specified_subscars test

--- test/src/org/sbols/converter/rsbpml/subparts/Expected_SpecifiedSubscarsTest.java
+++ test/src/org/sbols/converter/rsbpml/subparts/Expected_SpecifiedSubscarsTest.java
@@ -14,6 +14,7 @@ import org.sbols.converter.rsbpml.Rsbpml;
 import org.sbols.converter.util.ReadXML;
 import org.sbols.converter.util.DiffMatchPatch;
 import org.sbols.converter.util.ReadFile;
+import org.sbols.converter.util.WriteFile;
 import org.sbolstandard.core.SBOLDocument;
 import org.sbolstandard.core.SBOLFactory;
 
@@ -24,24 +25,21 @@ import org.sbolstandard.core.SBOLFactory;
 public class Expected_SpecifiedSubscarsTest {
 
     @Test
-    public void Valid_SpecifiedSubcarsTest() throws JAXBException, FileNotFoundException, IOException {
-        System.out.println("Expected_SpecifiedSubcarsTest");
+    public void Expected_SpecifiedSubscarsTest() throws JAXBException, FileNotFoundException, IOException {
+        System.out.println("Expected_SpecifiedSubscarsTest");
 
         //Get input for test
-        Rsbpml rsbpmlData = ReadXML.file("test/data/subparts/Valid_SpecifiedSubcarsTest.xml");
+        Rsbpml rsbpmlData = ReadXML.file("test/data/subparts/Valid_SpecifiedSubscarsTest.xml");
 
         //Do the test
         SBOLDocument SbolDoc = SBOLConverter.convert(rsbpmlData);
         String actual = ReadFile.sbolDocToString(SbolDoc);
 
         // Sometimes we generate the expected results using the test - cheating
-        //String file_out_path = "test/data/rdfout/Expected_SpecifiedSubcarsTest.sbol.xml";
-        //     FileOutputStream fout = new FileOutputStream(file_out_path+"1");
-        //     FileOutputStream fout = new FileOutputStream(file_out_path);
-        //     SBOLFactory.write(SbolDoc, fout);
+        WriteFile.toPath(SbolDoc, "test/data/rdfout/Out_SpecifiedSubscarsTest.sbol.xml");
         
         //Get expected result
-        String expected = ReadFile.fromPath("test/data/subparts/Valid_SpecifiedSubcarsTest.sbol.xml");
+        String expected = ReadFile.fromPath("test/data/subparts/Valid_SpecifiedSubscarsTest.sbol.xml");
 
         //Compare
         assertTrue("Diffs found: ", ReadFile.compare(expected, actual));
