edcfc6b5717264f703f07c206a8be2f4bbbc33fa
Michal Galdzicki <mgaldzic@gmail.com>
2012-12-31 13:38:01 -0800

    Subpart Features test

--- test/src/org/sbols/converter/rsbpml/subparts/Expected_FeaturesTest.java
+++ test/src/org/sbols/converter/rsbpml/subparts/Expected_FeaturesTest.java
@@ -18,20 +18,20 @@ import org.sbolstandard.core.SBOLDocument;
  *
  * @author mgaldzic
  */
-public class Expected_MultiDeepSubpartsTest {
+public class Expected_FeaturesTest {
 
     @Test
-    public void Expected_MultiDeepSubpartsTest() throws JAXBException, IOException {
-        System.out.println("Expected_MultiDeepSubpartsTest");
+    public void Expected_FeaturesTest() throws JAXBException, IOException {
+        System.out.println("Expected_FeaturesTest");
         //Get input for test
-        Rsbpml rsbpmlData = ReadXML.file("test/data/subparts/Valid_MultiDeepSubpartsTest.xml");
+        Rsbpml rsbpmlData = ReadXML.file("test/data/subparts/Valid_FeaturesTest.xml");
 
         //Do the test
         SBOLDocument SbolDoc = SBOLConverter.convert(rsbpmlData);
         String actual = ReadFile.sbolDocToString(SbolDoc);
         
         //Get expected result
-        String expected = ReadFile.fromPath("test/data/subparts/Valid_MultiDeepSubpartsTest.sbol.xml");
+        String expected = ReadFile.fromPath("test/data/subparts/Valid_FeaturesTest.sbol.xml");
 
         //Compare
         assertTrue("Diffs found: ", ReadFile.compare(expected, actual));
---
+++ test/src/org/sbols/converter/rsbpml/subparts/Valid_FeaturesTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.sbols.converter.rsbpml.subparts;
+
+/**
+ *
+ * @author mgaldzic
+ */
+
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import org.junit.Test;
+import org.sbols.converter.rsbpml.Rsbpml;
+import org.sbols.converter.util.ReadXML;
+import org.sbolstandard.core.DnaComponent;
+import org.sbolstandard.core.SBOLDocument;
+import org.sbolstandard.core.SBOLFactory;
+
+public class Valid_FeaturesTest {
+
+    @Test
+    public void Valid_FeaturesTest() throws JAXBException, IOException {
+        System.out.println("Valid_FeaturesTest");
+        Rsbpml rsbpmlData = ReadXML.file("test/data/subparts/Valid_FeaturesTest.xml");
+
+        SBOLDocument Doc = SBOLFactory.createDocument();
+        DnaComponent biobrick = rsbpmlData.toSbol();
+        Doc.addContent(biobrick);
+        SBOLFactory.validate(Doc);
+    }
+}
