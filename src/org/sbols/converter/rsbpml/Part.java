2663f531b0818fa97c76c23583ef4582408511fa
Michal Galdzicki <mgaldzic@gmail.com>
2013-01-07 16:18:01 -0800

    fixed \s+ split Sequence strings - test pass

--- src/org/sbols/converter/rsbpml/Part.java
+++ src/org/sbols/converter/rsbpml/Part.java
@@ -201,7 +201,7 @@ public class Part {
 
             for (String aSeq : seq_data) {
                 if (!aSeq.isEmpty()) {
-                    sequenceObject.setNucleotides(aSeq);
+                    sequenceObject.setNucleotides(aSeq.replaceAll("\\s+", ""));
                     sequenceObject.setURI(URI.create("http://partsregistry.org/seq/partseq_" + part_id)); //Need to make dynamic
                     biobrick.setDnaSequence(sequenceObject);
                 }
---
+++ test/src/org/sbols/converter/rsbpml/basic/Expected_SplitLineSequence_Test.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sbols.converter.rsbpml.basic;
+
+/**
+ *
+ * @author mgaldzic
+ */
+
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.sbols.converter.SBOLConverter;
+import org.sbols.converter.rsbpml.Rsbpml;
+import org.sbols.converter.util.ReadFile;
+import org.sbols.converter.util.ReadRSBPML;
+import org.sbols.converter.util.WriteFile;
+import org.sbolstandard.core.SBOLDocument;
+
+/**
+ *
+ * @author mgaldzic
+ */
+public class Expected_SplitLineSequence_Test {
+
+    @Test
+    public void Expected_SplitLineSequence_Test() throws JAXBException, IOException {
+        System.out.println("Expected_SequenceTest");
+        //Get input for test
+        Rsbpml rsbpmlData = ReadRSBPML.file("test/data/basic/SplitLineSequence_Test.xml");
+
+        //Do the test
+        SBOLDocument SbolDoc = SBOLConverter.convert(rsbpmlData);
+        String actual = ReadFile.sbolDocToString(SbolDoc);
+
+        // Sometimes we generate the expected results using the test - cheating
+        WriteFile.toPath(SbolDoc, "test/data/rdfout/SplitLineSequenceTest_Out.sbol.xml");
+
+        //Get expected result
+        String expected = ReadFile.fromPath("test/data/basic/SplitLineSequence_Expected.sbol.xml");
+
+        //Compare
+        assertTrue("Diffs found: ", ReadFile.compare(expected, actual));
+    }
+}
